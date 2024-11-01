327e2784eda1a6b123ff4aa3adab93ffc2d9718d
enisaydin <enisaydineng@gmail.com>
2024-11-01 05:10:53 +0300

    ASP.NET CORE WEB API
    A WheatherAPI project containing simple CRUD operations using ASP.NET CORE

---
+++ WeatherAPI/WeatherAPI/Context/WeatherContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection.Metadata.Ecma335;
+using WeatherAPI.Entities;
+
+namespace WeatherAPI.Context
+{
+    public class WeatherContext : DbContext 
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source =EA;Database=WeatherApiDB;User Id=sa;Password=123456;Trusted_Connection=true;");
+
+        }
+        public DbSet<City> Cities { get;set; }
+    }
+}
---
+++ WeatherAPI/WeatherAPI/Entities/City.cs
@@ -0,0 +1,11 @@
+﻿namespace WeatherAPI.Entities
+{
+    public class City
+    {
+        public int CityId { get; set; }
+        public string  CityName { get; set; }
+        public string Country { get; set; }
+        public decimal Temp { get; set; }
+        public string Detail { get; set; }
+    }
+}
